844beb23d9f144af9b25e2ef1b66ca4dea27acea
unknown <lfavoreto@hotmail.com>
2021-04-08 13:17:12 -0300

    Confg Mobile

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,11 +1,17 @@
+/****MASK****/
+
 $("#cel").mask("(00) 00000-0000");
 $("#cpf").mask("000.000.000-00");
+
+
+/*****EMAIL-VALIDATION******/
 
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
 
+/****BOTOES-DE-ETAPA****/
 
 $(function(){
     var et01 = document.getElementById('etapa01');
@@ -38,3 +44,13 @@ $(function(){
         }
     });
 });
+
+/***CONF-DE-MOBILE****/
+
+function switchElements($ele1, $ele2) {
+    $ele1.before($ele2);
+}
+
+if( $(window).width() <= 740){
+    switchElements($('#voltar'), $('#enviar'));
+}
