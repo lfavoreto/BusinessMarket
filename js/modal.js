3b2d7245ff9303ea357843b8498b5891263f44f8
unknown <lfavoreto@hotmail.com>
2021-04-09 13:10:24 -0300

    Modal

--- js/modal.js
+++ js/modal.js
@@ -1,30 +1,27 @@
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
-
-function apear() {
-    
-    var cpf = document.getElementById('cpf');
-    var cit = document.getElementById('cidade');
-    var sta = document.getElementById('estado');
-
-    var nome = document.getElementById('fullname').value;
-    var tel = document.getElementById('cel').value;
-
-    if(cpf.value && cit.value && sta.value){
-        modal.style.display = "block";
-        document.getElementById('no').innerHTML = nome;
-        document.getElementById('te').innerHTML = tel;
-        document.getElementById('cp').innerHTML = cpf.value;
-    }
-}
+let cont = 0;
 
 span.onclick = function() {
   modal.style.display = "none";
+  cont = 1;
 }
 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    cont = 1;
   }
 }
+
+$('body').mouseleave(function(){
+  if(cont == 0){
+    modal.style.display = "block";
+  }
+});
+
+
+/****MASK****/
+$("#tel").mask("(00) 00000-0000");
+$("#cpf").mask("000.000.000-00");
