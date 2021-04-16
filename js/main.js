90ca792d1b4da4d1b393f366528f703c06e7ebf1
unknown <lfavoreto@hotmail.com>
2021-04-16 10:38:21 -0300

    Transition

--- js/main.js
+++ js/main.js
@@ -29,3 +29,20 @@ function anteriorVideo(){
     x.style = 'display: block;';
     y.style = 'display: none;';
 }
+
+$(function(){
+
+    $('.back-to-top').hide();
+    $(window).scroll(function(){
+       if($(this).scrollTop() > 1000){
+            $('.back-to-top').fadeIn();
+       } else{
+           $('.back-to-top').fadeOut();
+       }
+    });
+
+    $(".back-to-top").click(function() {
+        $("html, body").animate({scrollTop: 0}, 800);
+    });
+});
+    
